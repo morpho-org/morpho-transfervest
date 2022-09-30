4aabfc7a18c812b9c32def25daad6bef76a4d80f
MerlinEgalite <egalite.merlin@gmail.com>
2022-09-30 11:15:19 +0200

    script: update vester deployer script

--- script/VesterDeployer.s.sol
+++ script/VesterDeployer.s.sol
@@ -7,11 +7,14 @@ import "forge-std/console.sol";
 import {Vester} from "src/Vester.sol";
 
 contract VesterDeployer is Script {
+    address public constant MORPHO_DAO = 0xcBa28b38103307Ec8dA98377ffF9816C164f9AFa;
+    address public constant MORPHO_TOKEN = 0x9994E35Db50125E0DF82e4c2dde62496CE330999;
+
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-        Vester vester = new Vester(msg.sender, address(1));
+        Vester vester = new Vester(MORPHO_DAO, MORPHO_TOKEN);
         console.log(address(vester));
         vm.stopBroadcast();
     }
