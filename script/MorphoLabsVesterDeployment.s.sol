58e6c2e5b2ef7d6eda428ba3f5c3d300951b694e
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-02 12:52:16 +0200

    script: add deployment scripts for vester contracts

--- script/MorphoLabsVesterDeployment.s.sol
+++ script/MorphoLabsVesterDeployment.s.sol
@@ -6,16 +6,16 @@ import "forge-std/console.sol";
 
 import {Vester} from "src/Vester.sol";
 
-contract VesterDeployer is Script {
-    address public constant MORPHO_DAO = 0xcBa28b38103307Ec8dA98377ffF9816C164f9AFa;
+contract MorphoLabsVesterDeployment is Script {
+    address public constant MORPHO_LABS = 0x1590e7F4c3E1B4493Abb462e34593aef3A9397Dd;
     address public constant MORPHO_TOKEN = 0x9994E35Db50125E0DF82e4c2dde62496CE330999;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-        Vester vester = new Vester(MORPHO_DAO, MORPHO_TOKEN);
-        console.log(address(vester));
+        Vester vester = new Vester(MORPHO_LABS, MORPHO_TOKEN);
         vm.stopBroadcast();
+        console.log(address(vester));
     }
 }
