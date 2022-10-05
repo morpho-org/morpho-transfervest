4423025c4e4cc3823b3f23959d076313e9932fac
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-05 21:18:32 +0200

    script: update naming

--- script/AdmoVesterDeployment.s.sol
+++ script/AdmoVesterDeployment.s.sol
@@ -6,7 +6,7 @@ import "forge-std/console.sol";
 
 import {Vester} from "src/Vester.sol";
 
-contract MorphoLabsVesterDeployment is Script {
+contract AdmoVesterDeployment is Script {
     address public constant ADMO = 0x6ABfd6139c7C3CC270ee2Ce132E309F59cAaF6a2;
     address public constant MORPHO_TOKEN = 0x9994E35Db50125E0DF82e4c2dde62496CE330999;
 
